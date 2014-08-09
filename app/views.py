7ed5b736e0489539d6defa27764fadc5972322dc
Wilson Qin <wilson.qin@gmail.com>
2014-08-09 18:52:47 -0400

    removed views extra code

--- app/views.py
+++ app/views.py
@@ -3,8 +3,3 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-
-def mongotest(request):
-    
-    return HttpResponse("RESULT OF MONGOTEST: " + )
