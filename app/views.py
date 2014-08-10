9f9f45030cbf528659c22d23aaa3c44c82891af5
Wilson Qin <wilson.qin@gmail.com>
2014-08-09 21:23:56 -0400

    login page stubbed

--- app/views.py
+++ app/views.py
@@ -6,6 +6,10 @@ def index(request):
 	context = {'title': 'adsfsa'}
 	return render(request, 'index.html', context)
 
+def login(request):
+  context = {'title': 'Login to Dotis'}
+  return render(request, 'login.html', context)
+
 def createDonation(request):
 	context = {'title': 'Create Donation'}
 	return render(request, 'donation.html', context)
