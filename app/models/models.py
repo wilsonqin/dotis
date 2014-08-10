40c1d975b9549734ae76896a722269a4afd65b52
Wilson Qin <wilson.qin@gmail.com>
2014-08-10 01:41:07 -0400

    tuned up model fields

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,9 @@
 from mongoengine import *
 import datetime
 
+#remember our User model is in its own file
+from User import User
+
 class Charity(Document):
   name = StringField()
   types_accepted = ListField()
@@ -12,7 +15,7 @@ class Charity(Document):
     }
 
 class DropoffLocation(Document):
-  charity_id = StringField()
+  charity = ReferenceField('Charity')
   location = PointField(auto_index=False)
 
   meta = {
@@ -31,27 +34,32 @@ class Donation(Document):
   # collection_id = StringField()
   # charity_id = StringField()
 
+pledge_status = ["pending", "done"]
 
 class Pledge(Document):
-  donation_id = StringField()
-  user_id = StringField()
+  donation = ReferenceField('Donation')
+  user = ReferenceField('User')
   amount = FloatField(0)
+  status = StringField(default="pending")
+  date_pledged = DateTimeField(default=datetime.datetime.now)
 
 # collection of donation objects
 class Collection(Document):
-  user_id = StringField()
-  donation_id_list = StringField()
-
-
+  collector = ReferenceField('User')
+  donation_list = ListField(ReferenceField('Donation'))
+  donors = ListField(ReferenceField('User'))
+  date_committed = DateTimeField(default=datetime.datetime.now)
 
 # event of donation pick up / drop off
 # status is 0 for donation created by user
 # status is 0 for donation picked up by collector
 # status is 0 for donation dropped off at charity by collector
 
+transaction_status = ["created", "picked_up", "dropped_off"]
+
 class Transaction(Document):
-  status = IntField()
-  donation_id = StringField()
-  user_id = StringField()
+  status = StringField()
+  donation = ReferenceField('Donation')
+  user = ReferenceField('User')
   date = DateTimeField(default=datetime.datetime.now)
 
